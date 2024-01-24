c30ca6d38176543952bed610a80dbec19ee6bd00
kusaka0324 <haruto.k.4230@gmail.com>
2024-01-24 22:50:08 +0900

    add : initial commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle"
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
+      <ModeToggle />
+    </div>
+  )
+}
